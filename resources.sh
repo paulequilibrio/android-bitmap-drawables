dde1ab58a64c3b4e0acfb38a5c2c7451b101b46e
Paulo Alexandre <paulequilibrio@gmail.com>
2021-07-31 11:26:15 -0300

    Fix shellcheck issues

--- resources.sh
+++ resources.sh
@@ -5,26 +5,32 @@
 #  icon.png (512x512px)
 #  splash.png (1200x1200px centralized picture on a 2208x2208px frame)
 
-function print_fail() { printf "\e[0;31m[ FAIL ] %s\n\e[0m" "$1"; }
-function print_info() { printf "\e[0;36m[ INFO ] %s\n\e[0m" "$1"; }
-function print_ok()   { printf "\e[0;32m[  OK  ] %s\n\e[0m" "$1"; }
+print_fail() { printf "\e[0;31m[ FAIL ] %s\n\e[0m" "$1"; }
+print_info() { printf "\e[0;36m[ INFO ] %s\n\e[0m" "$1"; }
+print_ok()   { printf "\e[0;32m[  OK  ] %s\n\e[0m" "$1"; }
 
-[[ $1 == "" ]] && ABSPATH="$(realpath .)" || ABSPATH="$(cd $1; pwd)"
+basepath="$1"
+
+if [ -d "$basepath" ]; then
+  ABSPATH="$(realpath "$basepath")"
+else
+  ABSPATH="$(realpath .)"
+fi
 
 BASE_ICON="${ABSPATH}/icon.png"
 BASE_SPLASH="${ABSPATH}/splash.png"
 ICONS_PATH="${ABSPATH}/android/icon"
 SPLASHES_PATH="${ABSPATH}/android/splash"
 
-mkdir -p $ICONS_PATH
-mkdir -p $SPLASHES_PATH
+mkdir -p "$ICONS_PATH"
+mkdir -p "$SPLASHES_PATH"
 
 
-function check_file() {
-  if [ ! -f $1 ]; then
+check_file() {
+  if [ ! -f "$1" ]; then
     print_fail "not found: $1" && exit 1
   else
-    DIMENSIONS=$(identify $1 | grep -oP '\d+x\d+(?=\+)')
+    DIMENSIONS=$(identify "$1" | grep -oP '\d+x\d+(?=\+)')
     if [ "$DIMENSIONS" != "$2" ]; then
       print_fail "wrong dimensions ($DIMENSIONS != $2): $1" && exit 2
     fi
@@ -32,7 +38,7 @@ function check_file() {
 }
 
 
-function create_icon() {
+create_icon() {
   case $1 in
        ldpi )  SIZE='36x36'  ;;
        mdpi )  SIZE='48x48'  ;;
@@ -42,12 +48,15 @@ function create_icon() {
     xxxhdpi ) SIZE='192x192' ;;
   esac
 
-  convert $BASE_ICON -resize ${SIZE} ${ICONS_PATH}/drawable-$1-icon.png 2> /dev/null \
-  && print_ok "$1" || print_fail "$1"
+  if convert "$BASE_ICON" -resize ${SIZE} "${ICONS_PATH}/drawable-$1-icon.png" 2> /dev/null ; then
+    print_ok "$1"
+  else
+    print_fail "$1"
+  fi
 }
 
 
-function create_splash() {
+create_splash() {
   case $1 in
        ldpi )  WIDTH=320;  HEIGHT=240 ;;
        mdpi )  WIDTH=480;  HEIGHT=320 ;;
@@ -58,17 +67,23 @@ function create_splash() {
   esac
 
   PERCENTAGE=$(echo "scale=2; 100*${WIDTH}/2208" | bc)
-  OFFSET=$(( ($WIDTH-$HEIGHT)/2 ))
+  OFFSET=$(( (WIDTH-HEIGHT)/2 ))
 
-  convert $BASE_SPLASH -resize ${PERCENTAGE}% -crop "${WIDTH}x${HEIGHT}+0+${OFFSET}" ${SPLASHES_PATH}/drawable-land-$1-screen.png 2> /dev/null \
-  && print_ok "$1 landscape" || print_fail "$1 landscape"
+  if convert "$BASE_SPLASH" -resize "${PERCENTAGE}%" -crop "${WIDTH}x${HEIGHT}+0+${OFFSET}" "${SPLASHES_PATH}/drawable-land-$1-screen.png" 2> /dev/null ; then
+    print_ok "$1 landscape"
+  else
+    print_fail "$1 landscape"
+  fi
 
-  convert $BASE_SPLASH -resize ${PERCENTAGE}% -crop "${HEIGHT}x${WIDTH}+${OFFSET}+0" ${SPLASHES_PATH}/drawable-port-$1-screen.png 2> /dev/null \
-  && print_ok "$1 portrait"  || print_fail "$1 portrait"
+  if convert "$BASE_SPLASH" -resize "${PERCENTAGE}%" -crop "${HEIGHT}x${WIDTH}+${OFFSET}+0" "${SPLASHES_PATH}/drawable-port-$1-screen.png" 2> /dev/null ; then
+    print_ok "$1 portrait"
+  else
+    print_fail "$1 portrait"
+  fi
 }
 
 
-check_file $BASE_ICON '512x512' && print_info 'Generating icon...'
+check_file "$BASE_ICON" '512x512' && print_info 'Generating icon...'
 create_icon ldpi
 create_icon mdpi
 create_icon hdpi
@@ -76,7 +91,7 @@ create_icon xhdpi
 create_icon xxhdpi
 create_icon xxxhdpi
 
-check_file $BASE_SPLASH '2208x2208' && print_info 'Generating splash...'
+check_file "$BASE_SPLASH" '2208x2208' && print_info 'Generating splash...'
 create_splash ldpi
 create_splash mdpi
 create_splash hdpi
